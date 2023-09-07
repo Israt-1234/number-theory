1384ee9cc7125e57ad4e764fcafa42a2abcbbcf4
Israt jahan <140290748+Israt-1234@users.noreply.github.com>
2023-09-07 10:20:10 +0600

    Update Primality Test.cpp

--- Primality Test.cpp
+++ Primality Test.cpp
@@ -23,3 +23,36 @@ int main()
     }
     return 0;
 }
+
+
+
+// using long long
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+bool prime(ll n)
+{
+    if(n<2) return false;
+    if(n<=3) return true;
+    if(n%2==0) return false;
+    for(int i=3;i<=sqrt(n);i+=2)
+    {
+        if(n%i==0)
+            return false;
+    }
+         return true;
+}
+int main()
+{
+    ll n;
+    cin>>n;
+    while(n--){
+        ll t;
+      cin>>t;
+      if(prime(t)==true)
+       cout<<"yes"<<endl;
+       else
+       cout<<"no"<<endl;
+    }
+    return 0;
+}
