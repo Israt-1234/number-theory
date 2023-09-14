e79bce0c21d8ce8b343fe2b31a564d9c0f468fc9
Israt jahan <140290748+Israt-1234@users.noreply.github.com>
2023-09-14 16:31:14 +0600

    Add files via upload
    number theory problem

---
+++ OPMODULO Operation  Modulo.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+ll modulu(ll l ,ll r)
+{
+    ll sum=0;
+    ll n=1e9+7;
+   ll t=r-l;
+   t=t*(t/2);
+  cout<< t<<endl;
+}
+int main()
+{
+    ll l,r;
+    cin>>l>>r;
+    modulu(l,r-1);
+}
