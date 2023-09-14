8f7b5d5cdb779a2b90048e1e66f5017f859fff60
Israt jahan <140290748+Israt-1234@users.noreply.github.com>
2023-09-14 19:51:29 +0600

    Update sieve of eratosthenes number theory.cpp

--- sieve of erstosthenes number theory.cpp
+++ sieve of erstosthenes number theory.cpp
@@ -4,7 +4,7 @@ void sieve(int n)
 {
     int prime[n+3];
     memset(prime,0,sizeof(prime));
-    for(int i=2;i<=n;i++){
+    for(int i=2;i*i<=n;i++){
         if(prime[i]==0){
             for(int j=i*i;j<=n;j+=i){
                 prime[j]=1;
